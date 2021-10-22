8c9ec7060380769f9af72610dc015a52daeed716
hidsh <hideaki.sh@gmail.com>
2021-10-22 12:12:55 +0900

    add page "post_new"

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,18 @@ def post_detail(request, pk):
 	
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+	from .forms import PostForm
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			from django.shortcuts import redirect
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
