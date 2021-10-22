66d8d072faa1f08bc873733fdc40bbb372bc5b7c
hidsh <hideaki.sh@gmail.com>
2021-10-22 23:26:27 +0900

    add "edit_post"

--- blog/views.py
+++ blog/views.py
@@ -12,8 +12,28 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_edit(request, pk):
+	from django.shortcuts import render, get_object_or_404
+	from .forms import PostForm
+	from django.shortcuts import redirect
+	
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_new(request):
 	from .forms import PostForm
+	from django.shortcuts import redirect
+
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
@@ -21,7 +41,6 @@ def post_new(request):
 			post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
-			from django.shortcuts import redirect
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
