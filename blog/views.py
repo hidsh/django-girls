c5520ec0c733c62e6f205db8ca993f49649cf24a
hidsh <hideaki.sh@gmail.com>
2021-10-23 11:38:03 +0900

    add button "publish" for unpublish post

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})
 
 def post_detail(request, pk):
 	from django.shortcuts import render, get_object_or_404
@@ -23,12 +27,20 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			# post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_publish(request, pk):
+	from django.shortcuts import render, get_object_or_404
+	from django.shortcuts import redirect
+
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
 
 def post_new(request):
 	from .forms import PostForm
